10e8f3158f2143552580aa72c28fdc91cf89a39c
Микола <hulkovskij@gmail.com>
2024-03-12 21:13:08 +0200

    Added started table

--- src/TableCreator.php
+++ src/TableCreator.php
@@ -4,8 +4,33 @@ namespace YourNamespace;
 
 class TableCreator
 {
-    public function sayHello()
-    {
-        return "Hello, World! Dear";
+    private $headers;
+    private $data;
+
+    public function __construct($headers, $data) {
+        $this->headers = $headers;
+        $this->data = $data;
+    }
+
+    public function renderTable() {
+        echo '<table border="1">';
+
+        // Render headers
+        echo '<tr>';
+        foreach ($this->headers as $header) {
+            echo '<th>' . $header . '</th>';
+        }
+        echo '</tr>';
+
+        // Render data
+        foreach ($this->data as $row) {
+            echo '<tr>';
+            foreach ($row as $value) {
+                echo '<td>' . $value . '</td>';
+            }
+            echo '</tr>';
+        }
+
+        echo '</table>';
     }
 }
